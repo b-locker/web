6a53dc6a451f22f5bd896b55d5ba290531502742
Thijs W <mawijnen2@student.avans.nl>
2019-12-12 15:17:06 +0100

    Finished header component

--- b-locker-web/src/components/users/header/userHeader.tsx
+++ b-locker-web/src/components/users/header/userHeader.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './userHeader.scss';
+import logo from '../../../assets/qr.png'
 
 const UserHeader: React.FC = () => {
     return(
         <div className="header">
-            <p>B-locker</p>
+            <p id="title">B-locker</p>
+            <img src={logo} alt="" id="logo" />
         </div>
     );
 }
