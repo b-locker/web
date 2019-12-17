3aba99ff25a9ceb57795f983747e49a7645b96c2
Bram <Bram.mathijssen@gmail.com>
2019-12-17 14:55:55 +0100

    renamed topbar and dashboard changes

---
+++ b-locker-web/src/components/organisation/dashboard/orgDashboard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './orgDashboard.scss';
+import orgNavigation from '../navigation/orgNavigation';
+
+
+const orgDashboard: React.FC = () => {
+
+    return (
+        <main>
+            <h1>Dashboard</h1>
+            <div className="container">
+
+            </div>
+        </main>
+    );
+}
+
+export default orgDashboard;
