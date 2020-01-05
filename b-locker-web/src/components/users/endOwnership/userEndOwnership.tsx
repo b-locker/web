c91e38d578a1a499b5375d697a83fc12970e55b3
Thijs W <mawijnen2@student.avans.nl>
2020-01-05 16:35:05 +0100

    enhanced template. added translations and started end ownership page

--- b-locker-web/src/components/users/endOwnership/userEndOwnership.tsx
+++ b-locker-web/src/components/users/endOwnership/userEndOwnership.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import UserHeader from '../header/userHeader'
+import { useTranslation } from 'react-i18next';
 
 const UserEndOwnership: React.FC = () => {
+    const { t } = useTranslation();
 
-    return(
-        <div>
-            UserEndOwnership
+    return (
+        <div className="main-div">
+            <UserHeader />
+            <div className="global-edge-div">
+                <div className="global-title-desc-div ">
+                    <p className="global-page-title ">{t('end.areyousure.label')}</p>
+                    <p className="global-desc-label ">{t('end.desc.label')}</p>
+                </div>
+            </div>
         </div>
     );
 }
--- b-locker-web/src/templates/template.tsx
+++ b-locker-web/src/templates/template.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import UserHeader from '../components/users/header/userHeader'
+import { useTranslation } from 'react-i18next';
 
 const templateName: React.FC = () => {
+    const { t } = useTranslation();
 
-    return(
-        <div>
-
+    return (
+        <div className="main-div">
+            <UserHeader />
+            <div className="global-edge-div">
+                <div className="global-title-desc-div ">
+                    <p className="global-page-title ">{t('hello.label')}</p>
+                    <p className="global-desc-label ">{t('thankyou.label')}</p>
+                </div>
+            </div>
         </div>
     );
 }
