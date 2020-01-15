e652188e8b55f5c7bd8f90abd6a5074713074f77
Thijs W <mawijnen2@student.avans.nl>
2020-01-15 15:58:21 +0100

    change api url to //

--- b-locker-web/src/global/http/httpProvider.tsx
+++ b-locker-web/src/global/http/httpProvider.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export class httpProvider {
-    apiURL: string = "http://b-locker.nl:8080/api/v1"
+    apiURL: string = "//b-locker.nl:8080/api/v1"
     public getRequest(url: string, jwt?: string): Promise<any> {
         if (jwt) {
             const options = {
